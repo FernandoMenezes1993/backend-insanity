b8bbdd03c352bfe6f8de94a001ac68ccbd5a51a5
Fernando <fernando.menezes@conversion.com.br>
2024-05-01 15:23:02 -0300

    feat: criando variaveis de anbiente no sistema

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
+require('dotenv').config({path: '.env'});
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./router');
 const mongoose = require("mongoose");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const porta = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -14,8 +16,8 @@ app.use('/api', router);
 mongoose.connect("mongodb+srv://insanityalbion:Xb8qqRnNTrDFrfAr@insanity-api.xswedqc.mongodb.net/?retryWrites=true&w=majority&appName=insanity-api", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Conexão com MongoDB estabelecida com sucesso!");
-    app.listen(port, () => {
-      console.log(`Servidor rodando na porta ${port}`);
+    app.listen(porta, () => {
+      console.log(`Servidor rodando na porta ${porta}`);
     });
   })
   .catch(err => {
