da727fc1c30d93718ca1ac418f98ac6a8ee14ace
Fernando <fernando.menezes@conversion.com.br>
2024-05-01 19:49:08 -0300

    feat: Ajustando urls no cors

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
+require('dotenv').config({path: '.env'});
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 
 const userControllers = require('./users/userControllers');
 
+const WhiteList = process.env.URL_LOCAL || "https://insanity-roan.vercel.app/"
 var corsOptions = {
     header:{
-        'Access-Control-Allow-Origin': 'http://localhost:5173'
+        'Access-Control-Allow-Origin': `${WhiteList}`
     },
     origin: 'http://localhost:5173',
     optionsSuccessStatus: 200 
@@ -15,12 +17,12 @@ var corsOptions = {
 
 router.get("/user", userControllers.getAllUsers);
 
-router.get("/playresGuild", userControllers.getAllMembres);
+router.get("/playresGuild", cors(corsOptions),userControllers.getAllMembres);
 //Pegar o nome e ID de todos os membros da Insanity BR
 
 //POST
 
-router.post("/user/new", userControllers.addNewUser);
+router.post("/user/new", cors(corsOptions), userControllers.addNewUser);
 //Cadastrar novo usuario
 
 
